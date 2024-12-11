e7022d43b45fdd3cb4b24dfd9f3712ad56f54026
VIDHI SHAW <shawvidhi@gmail.com>
2024-12-11 15:14:25 +0000

    entrypoint updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,17 @@
 
 echo "============="
 
+# Configure Git
 git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${INPUT_EMAIL}"
 git config --global --add safe.directory /github/workspace
 
-python3 /urs/bin/feed.py
+# Run the Python script
+python3 /usr/bin/feed.py
 
-git add -A && git commit -m "Update Feed"
+# Stage, commit, and push changes
+git add -A
+git commit -m "Update Feed" || echo "No changes to commit."
 git push --set-upstream origin main
-
 
 echo "============="
